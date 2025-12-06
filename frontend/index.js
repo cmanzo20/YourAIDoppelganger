e645e189c2572e691149c521c1a710746f1553c3
Carlos <cmanzo20@stu.jjc.edu>
2025-12-05 22:23:03 -0600

    Removed export AI model tab, functionality is unnecessary and doesn't make sense for this project

--- frontend/index.js
+++ frontend/index.js
@@ -3,7 +3,6 @@ function showTab(tabId) {
   document.getElementById('train').style.display = 'none';
   document.getElementById('generate').style.display = 'none';
   document.getElementById('manage').style.display = 'none';
-  document.getElementById('save').style.display = 'none';
   document.getElementById(tabId).style.display = 'block';
 
   if (tabId === "manage") {
